6f29f0eec251451a6083c1a3c30513e9359627ee
cllorca1 <carlos.llorca@tum.de>
2017-03-24 10:06:44 +0100

    eventAnalyzer fileNames as args

--- src/main/java/org/matsim/munichArea/outputCreation/tripDurationAnalyzer/AllModesTripAnalyzer.java
+++ src/main/java/org/matsim/munichArea/outputCreation/tripDurationAnalyzer/AllModesTripAnalyzer.java
@@ -17,51 +17,13 @@ public class AllModesTripAnalyzer {
         File propFile = new File("munich.properties");
         rb = ResourceUtil.getPropertyBundle(propFile);
 
-        AgentTripDurationEventAnalyzer analyzer = new AgentTripDurationEventAnalyzer(rb);
+        for (String eventsFileName : args) {
 
-        //cases for trip sampling analysis
-//        String eventsFileName = "C:/projects/MATSim/scaling/events/TF0.05CF0.05SF0.05IT20_2016.output_events.xml.gz";
-//        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-//        analyzer.writeListTripDurations("C:/projects/MATSim/scaling/events/5");
-//
-//        analyzer = new AgentTripDurationEventAnalyzer(rb);
-//        eventsFileName = "C:/projects/MATSim/scaling/events/TF0.10CF0.10SF0.10IT20_2016.output_events.xml.gz";
-//        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-//        analyzer.writeListTripDurations("C:/projects/MATSim/scaling/events/10");
-//
-//        analyzer = new AgentTripDurationEventAnalyzer(rb);
-//        eventsFileName = "C:/projects/MATSim/scaling/events/TF0.50CF0.50SF0.50IT20_2016.output_events.xml.gz";
-//        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-//        analyzer.writeListTripDurations("C:/projects/MATSim/scaling/events/50");
-//
-//        analyzer = new AgentTripDurationEventAnalyzer(rb);
-//        eventsFileName = "C:/projects/MATSim/scaling/events/TF1.00CF1.00SF1.00IT20_2016.output_events.xml.gz";
-//        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-//        analyzer.writeListTripDurations("C:/projects/MATSim/scaling/events/100");
+            AgentTripDurationEventAnalyzer analyzer = new AgentTripDurationEventAnalyzer(rb);
+            analyzer.runAgentTripDurationAnalyzer(eventsFileName);
+            analyzer.writeListTripDurations(eventsFileName);
 
-
-
-        String eventsFileName = "C:/models/munich/outputTF0,05CF0,10SF0,18IT20TestAV2/TestAV2_2016.output_events.xml.gz";
-        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-        analyzer.writeListTripDurations(eventsFileName);
-
-        analyzer = new AgentTripDurationEventAnalyzer(rb);
-
-        eventsFileName = "C:/models/munich/cottbus_robotaxi/output/robotaxi_munich4/output_events.xml.gz";
-        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-        analyzer.writeListTripDurations(eventsFileName);
-
-        analyzer = new AgentTripDurationEventAnalyzer(rb);
-
-        eventsFileName = "C:/models/munich/cottbus_robotaxi/output/robotaxi_munich8/output_events.xml.gz";
-        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-        analyzer.writeListTripDurations(eventsFileName);
-
-        analyzer = new AgentTripDurationEventAnalyzer(rb);
-
-        eventsFileName = "C:/models/munich/cottbus_robotaxi/output/robotaxi_munich12/output_events.xml.gz";
-        analyzer.runAgentTripDurationAnalyzer(eventsFileName);
-        analyzer.writeListTripDurations(eventsFileName);
+        }
 
     }
 }
